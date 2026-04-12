ea5b88d482b14a22ec499cc391c5528f13a4a1e2
davinzanaf <davinza203@gmail.com>
2026-04-12 22:08:53 +0700

    menambahkan style dihalaman dashboard dan login admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,16 +1,30 @@
 <?php
 session_start();
+
 if(!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
     header("Location: login.php");
     exit;
 }
 ?>
 
-<h2>Dashboard Admin</h2>
-<a href="logout.php">Logout</a>
-<br><br>
-<a href="tarif.php">Kelola Tarif</a>
-<br>
-<a href="kurir.php">Kelola Kurir</a>
-<br>
-<a href="pesanan.php">Data pesanan</a>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard Admin</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+<div class="container">
+    <h2>Dashboard Admin</h2>
+    <p>Selamat datang, <?php echo isset($_SESSION['nama_lengkap']) ? $_SESSION['nama_lengkap'] : 'Admin'; ?></p>
+
+    <div class="menu">
+        <a href="kurir.php">Kelola Kurir</a>
+        <a href="tarif.php">Kelola Tarif</a>
+        <a href="pesanan.php">Data Pesanan</a>
+        <a href="logout.php">Logout</a>
+    </div>
+</div>
+</body>
+</html>
--- admin/login.php
+++ admin/login.php
@@ -12,28 +12,36 @@ if(isset($_POST['login'])) {
     if($data && password_verify($password, $data['password'])) {
         $_SESSION['id_user'] = $data['id_user'];
         $_SESSION['role'] = $data['role'];
+        $_SESSION['nama_lengkap'] = $data['nama_lengkap'];
+
         header("Location: dashboard.php");
         exit;
     } else {
-        $error = "Email atau Password salah!";
+        $error = "Email atau password salah!";
     }
 }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="id">
 <head>
+    <meta charset="UTF-8">
     <title>Login Admin</title>
+    <link rel="stylesheet" href="../assets/style.css">
 </head>
 <body>
+<div class="container">
     <h2>Login Admin</h2>
 
-    <?php if(isset($error)) echo "<p style='color:red;'>$error</p>"; ?>
+    <?php if(isset($error)) { ?>
+        <div class="error-box"><?php echo $error; ?></div>
+    <?php } ?>
 
     <form method="POST">
-        <input type="email" name="email" placeholder="Email" required><br><br>
-        <input type="password" name="password" placeholder="Password" required><br><br>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
         <button type="submit" name="login">Login</button>
     </form>
+</div>
 </body>
 </html>
